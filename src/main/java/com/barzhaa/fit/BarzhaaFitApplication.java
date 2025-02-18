e3e0175c4d813467dee4844dfe8dbb0cacb02872
sharimardinov <sharimardinov@icloud.com>
2025-02-18 20:58:57 +0300

    letsgo

---
+++ src/main/java/com/barzhaa/fit/BarzhaaFitApplication.java
@@ -0,0 +1,13 @@
+package com.barzhaa.fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarzhaaFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarzhaaFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barzhaa/fit/BarzhaaFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barzhaa.fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarzhaaFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
